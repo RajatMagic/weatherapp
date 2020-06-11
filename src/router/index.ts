e8426a1296daf040573356e968aff3135fe43545
Rajat Sehdev <rajat.sehdev@magicsw.com>
2020-06-11 17:16:07 +0530

    route file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './route';
-import APP_UTILITIES from "@/utilities/commonFunctions";
-import APP_CONST from '@/constants/AppConst';
 Vue.use(VueRouter)
 
 const router = new VueRouter({
